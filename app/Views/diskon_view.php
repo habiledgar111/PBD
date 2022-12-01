5d3817dc4f447cfdcb7508285829141f3e111cd5
garr007 <103429027+garr007@users.noreply.github.com>
2022-12-02 00:11:17 +0700

    Update diskon_view.php

--- app/Views/diskon_view.php
+++ app/Views/diskon_view.php
@@ -1,16 +1,33 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Diskon</title>
+    <link rel="stylesheet" href="<?= base_url('public/css/style.css'); ?>">
 </head>
+
 <body>
-<form action="/home/diskon" method="post" >
-        <input type="number" name="jumlah" placeholder="jumlah">
-        <input type="text" name="harga" placeholder="harga">
-        <button type="sumbit" name="sumbit">save</button>
-    </from>
+    <div class="cool-form">
+        <div class="container">
+            <form " method=" post" action="/home/update_barang" enctype=" multipart/form-data">
+                <div class="input-item">
+                    <label>Jumlah </label>
+                    <input type="text" placeholder="Jumlah" name="jumlah">
+                </div>
+                <div class="input-item">
+                    <label>Harga </label>
+                    <input type="text" placeholder="Harga" name="harga">
+                </div>
+
+                <div class="input-item">
+                    <button>Save</button>
+                </div>
+            </form>
+        </div>
+    </div>
 </body>
+
 </html>
