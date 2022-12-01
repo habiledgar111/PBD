7a3306b550c83949c5c7385dfc1543f671f2b9ae
garr007 <103429027+garr007@users.noreply.github.com>
2022-12-02 00:10:18 +0700

    Update delete_view.php

--- app/Views/delete_view.php
+++ app/Views/delete_view.php
@@ -1,17 +1,33 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Diskon</title>
+    <link rel="stylesheet" href="<?= base_url('public/css/style.css'); ?>">
 </head>
-<body>
-<form action="/home/delete_barang" method="post" >
 
-        <input type="text" name="nama_table" placeholder="table">
-        <input type="number" name="id" placeholder="id">
-        <button type="sumbit" name="sumbit">save</button>
-    </from>
+<body>
+    <div class="cool-form">
+        <div class="container">
+            <form " method=" post" action="/home/update_barang" enctype=" multipart/form-data">
+                <div class="input-item">
+                    <label>Nama Table </label>
+                    <input type="text" placeholder="Nama Table" name="nama_table">
+                </div>
+                <div class="input-item">
+                    <label>id </label>
+                    <input type="text" placeholder="ID Barang" name="id">
+                </div>
+
+                <div class="input-item">
+                    <button>Save</button>
+                </div>
+            </form>
+        </div>
+    </div>
 </body>
+
 </html>
