8c2e10feb90bc30b8a4f3929a566b3cd72281702
garr007 <103429027+garr007@users.noreply.github.com>
2022-12-02 00:11:55 +0700

    Update insert_barang.php

--- app/Views/insert_barang.php
+++ app/Views/insert_barang.php
@@ -1,19 +1,47 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Insert Barang</title>
+    <link rel="stylesheet" href="<?= base_url('public/css/style.css'); ?>">
 </head>
+
 <body>
-    <form action="/home/insert_barang" method="post" >
-        <input type="text" name="nama_table" placeholder="table">
-        <input type="number" name="id" placeholder="id">
-        <input type="text" name="nama_barang" placeholder="nama barang">
-        <input type="number" name="jumlah" placeholder="jumlah">
-        <input type="text" name="harga" placeholder="harga">
-        <button type="sumbit" name="sumbit">save</button>
-    </from>
+    <div class="cool-form">
+        <div class="container">
+            <form " method=" post" action="/home/update_barang" enctype=" multipart/form-data">
+                <div class="input-item">
+                    <label>Nama Table </label>
+                    <input type="text" placeholder="Nama Table" name="nama_table">
+                </div>
+                <div class="input-item">
+                    <label>ID Barang </label>
+                    <input type="text" placeholder="ID Barang" name="id">
+                </div>
+
+                <div class="input-item">
+                    <label>Nama Barang </label>
+                    <input type="text" placeholder="Nama Barang" name="nama_barang">
+                </div>
+
+                <div class="input-item">
+                    <label>Jumlah </label>
+                    <input type="text" placeholder="Jumlah " name="jumlah">
+                </div>
+
+                <div class="input-item">
+                    <label>Harga </label>
+                    <input type="text" placeholder="Harga" name="harga">
+                </div>
+                <div class="input-item">
+                    <button>Save</button>
+                </div>
+            </form>
+        </div>
+    </div>
 </body>
+
 </html>
